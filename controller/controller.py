b7315cd81c9a044246d25328a61160cb9a75d039
SimonCooper236 <2441260@cegplimoilou.ca>
2025-12-14 11:14:10 -0500

    Refait la classe model.py et ajuster la classe controller.py model: planet est un objet, play, pause, stop est dans controller controller: ajouter play, pause, stop, ajouter Qtimer

--- controller/controller.py
+++ controller/controller.py
@@ -1,3 +1,5 @@
+from PyQt6.QtCore import QTimer
+
 from model import model
 from view import view
 
@@ -11,21 +13,33 @@ class Simulation_controller():
         self.__model = model
         self.__view = view
 
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.update)
+        self.running = False
+
         self.__view.play_pushButton.clicked.connect(self.play)
         self.__view.pause_pushButton.clicked.connect(self.pause)
         self.__view.stop_pushButton.clicked.connect(self.stop)
 
-        self.__model.model_changed.connect(self.__view.paintEvent)
-
 
     def ajout_object(self, object):
-        self.__model.ajout_object(object)
+        self.__model.add_planet(object)
 
     def play(self):
-        self.__model.play_simulation()
+        if not self.timer.stop():
+            self.timer.start(16)
+            self.running = True
 
     def pause(self):
-        self.__model.pause_simulation()
+        self.timer.stop()
+        self.running = False
 
     def stop(self):
-        self.__model.stop_simulation()
+        self.timer.stop()
+        self.running = False
+        self.__model.reset()
+        self.__view.update()
+
+    def update(self):
+        self.__model.step(1/60)
+        self.__view.update()
--- model/model.py
+++ model/model.py
@@ -1,11 +1,4 @@
-import math
-import threading
-import time
-
 import pymunk
-from PyQt6.QtCore import pyqtSignal, QThread, QObject
-
-from test import screen
 
 """
             Truc a faire / fonctionalité requise
@@ -16,99 +9,41 @@ from test import screen
  4- ajout d'un object
 
  """
-class Thread(threading.Thread):
-    etat = pyqtSignal(bool)
+class Planet:
+    def __init__(self, x, y, mass, radius, vx, vy):
+        self.body = pymunk.Body(mass, pymunk.moment_for_circle(mass, 0, radius))
+        self.body.position = (x, y)
+        self.body.velocity = (vx, vy)
 
-    def run(self):
-        self.etat.emit(True)
+        self.shape = pymunk.Circle(self.body, radius)
 
 
-
-class Simulation_model(QObject):
-    planets = []
-    model_changed = pyqtSignal(list)
-    running = False
-
+class Simulation_model():
     def __init__(self):
-        super().__init__()
-        self.create_sim()
-
-    def create_sim(self):
         self.space = pymunk.Space()
         self.space.gravity = (0, 0)
-        self.default_planets()
-        self.simuler()
+        self.planets = []
+        self.time = 0
 
-        self.model_changed.emit(self.planets)
+    def add_planet(self, x=0, y=0, mass=10, radius=6, vx=0, vy=0):
+        self.planets.append(Planet(x, y, mass, radius, vx, vy))
 
-    def default_planets(self):
-        self.add_planet()
-        self.add_planet(120, 0, 0, 60, 20, 10, "rouge")
-
-    def add_planet(self, x = 0, y = 0, vx = 0, vy = 10, masse = 10, rayon = 12, couleur = "bleu"):
-        body = pymunk.Body(masse, pymunk.moment_for_circle(masse, 0, rayon))
-        body.position = (x, y)
-        body.velocity = (vx, vy)
-
-        shape = pymunk.Circle(body, rayon)
-        shape.color = couleur
-
-        self.space.add(body, shape)
-        self.planets.append(body)
-
-        self.model_changed.emit(self.planets)
+    def apply_gravity(self):
+        for i, p1 in enumerate(self.planets):
+            for p2 in self.planets[i+1:]:
+                delta = p2.body.position - p1.body.position
+                dist = delta.length + 1e-5
+                force = 400 * p1.body.mass * p2.body.mass / dist**2
+                direction = delta.normalized()
+                p1.body.apply_force_at_world_point(force * direction, p1.body.position)
+                p2.body.apply_force_at_world_point(-force * direction, p2.body.position)
 
     def step(self, dt):
-        G = 2000
-        n = len(self.planets)
-        for i in range(n):
-            for j in range(i + 1, n):
-                p1 = self.planets[i]
-                p2 = self.planets[j]
-                b1 = p1.body
-                b2 = p2.body
-
-                dx = b2.position.x - b1.position.x
-                dy = b2.position.y - b1.position.y
-                dist_sqrt = dx * dx + dy * dy
-
-                if dist_sqrt == 0:
-                    continue
-
-                dist = math.sqrt(dist_sqrt)
-                force_grav = G * b1.mass * b2.mass/ dist_sqrt
-
-                fx = force_grav * dx / dist
-                fy = force_grav * dy / dist
-
-                b1.apply_force_at_local_point(fx, fy)
-                b2.apply_force_at_local_point(-fx, -fy)
-
+        self.time += dt
+        self.apply_gravity()
         self.space.step(dt)
 
-    def reset_model(self):
-        self.__init__()
-
-    def simuler(self):
-        running = True
-        self.simulation = Thread()
-        self.simulation.etat.connect(self.etat)
-        self.simulation.finished.connect(self.reset_model)
-        self.simulation.start()
-
-    def etat(self, etat):
-        if etat == False:
-            pass
-            #pause sim/ show pause text
-        else:
-            self.step(1/60)
-
-    def play_simulation(self):
-        print("play")
-
-
-    def pause_simulation(self):
-        print("pause")
-
-    def stop_simulation(self):
-        print("stop")
+    def reset(self):
+        self.space.remove(*self.planets)
+        self.planets.clear()
+        self.time = 0
